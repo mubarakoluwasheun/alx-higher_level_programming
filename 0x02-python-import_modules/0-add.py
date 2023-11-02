bfe2f6e5021e0a86276e2fe4df9c7e0d7b8c79e3
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-02 16:43:30 -0500

    imports add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
 
-print(f"{a} + {b} = {result}\n")
+if __name__ == "__main__":
+    """Main function that uses the add function from add_0 module."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}\n")
