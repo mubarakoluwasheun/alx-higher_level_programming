54afec479cf55f54cde48a3e0a6b724b9b57bf85
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-02 16:22:08 -0500

    add function import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a, b)
+a = 1
+b = 2
+result = add(a, b)
 
-    print(f"{a} + {b} = {result}\n")
+print(f"{a} + {b} = {result}\n")
