e79f5fdfa1fc45acd1320f1d471b9508d5553c5e
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-28 22:44:26 +0100

    a function def copy_list(l): that returns a copy of a list.

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
-    """
-    This function returns a copy of the input list.
-    :param l: List to be copied
-    :return: Copy of the input list
-    """
     return l[:]
