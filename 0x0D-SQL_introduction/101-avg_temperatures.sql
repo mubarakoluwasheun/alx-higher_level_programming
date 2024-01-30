96b5581f5249d0bce5f9653ab077c8b07bc1b37e
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-01-30 00:58:10 +0000

    My SQL

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Displays the average temperature (Fahrenheit) by city
 -- Ordered by temperature (descending)
 SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperature`
+FROM `temperatures`
 GROUP BY `city`
 ORDER BY `avg_temp` DESC;
