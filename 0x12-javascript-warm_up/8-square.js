32b303917e52f7c58cc3b133f8c765c7711d4204
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-02-16 00:59:54 +0100

    Javascript warm-up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const size = Math.floor(Number(process.argv[2]));
-if (isNAN(size)) {
-	console.log('Missing size');
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < size; i++) {
-		let sqr = '';
-		for (let j = 0; j < size; j++) sqr += 'X';
-		console.log(sqr);
-	}
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
