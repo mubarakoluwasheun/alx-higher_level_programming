b91799680bf7be7a124ee783d1ac3c8e60c8804b
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-02-17 17:37:38 +0000

    JS objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,11 +2,12 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,6 +2,6 @@
 
 let num = 0;
 exports.logMe = function (item) {
-  num ++;
+  num++;
   console.log(num + ': ' + item);
 };
