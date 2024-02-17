767bdddde2969d34ccde6c03c5653416d13f95fa
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-02-17 23:40:29 +0000

    JS objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,10 @@ const dict = require('./101-data').dict;
 
 const newDict = {};
 for (const key in dict) {
-    if (newDict[dict[key]] === undefined) {
-        newDict[dict[key]] = [];
-    }
-    newDict[dict[key]].push(key);
+  if (newDict[dict[key]] === undefined) {
+  newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
 }
 
 console.log(newDict);
