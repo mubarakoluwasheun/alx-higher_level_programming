4c13ba8bf2d90501123cc76b67bea821c132361b
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2024-02-19 05:53:44 +0100

    object relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 takes in the name of a state as an argument and lists all cities
-of that state, using the database hbtn_0e_4_usa
 Arguments: username, password, database, state name
 """
 
